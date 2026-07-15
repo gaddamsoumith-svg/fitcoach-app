cc07776614869488e84d15d0f3493b205490df23
gaddamsoumith-svg <gaddamsoumith@gmail.com>
2026-07-15 17:56:31 -0500

    Simplify rendering by removing surrounding div
    Removed the surrounding div to simplify the rendering structure.

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import FitCoachApp from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", padding: "16px 0" }}>
-      <FitCoachApp />
-    </div>
+    <FitCoachApp />
   </React.StrictMode>
 );
